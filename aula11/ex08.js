a5828201ef9c782e0133216d84153b7062f0b4f6
Bryan Lucas da Conceição Afinovicz <bryanafinovicz706@gmail.com>
2026-03-17 19:24:06 -0300

    ex08 e ex09

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+const velocidade = 61
+console.log(`A velocidade atingida foi de ${velocidade}km/h`)
+if(velocidade > 60){ // Condição Simples!
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO`)
+}
+console.log('Dirija sempre usando cinto de segurança')
