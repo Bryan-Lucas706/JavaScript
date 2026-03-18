6f4f2855c9c02356a5a2dd85e92dcadd9e3289c7
Bryan Lucas da Conceição Afinovicz <bryanafinovicz706@gmail.com>
2026-03-18 11:53:35 -0300

    aula 12 ex11, 12 e 13

---
+++ aula12/ex11.js
@@ -0,0 +1,12 @@
+const idade = 40;
+console.log(`Você tem ${idade} anos.`)
+
+if (idade < 16) {
+  console.log("Não vota");
+} 
+else if (idade < 18 || idade > 66) {
+  console.log("Voto opcional");
+} 
+else {
+  console.log("Voto obrigatorio");
+}
