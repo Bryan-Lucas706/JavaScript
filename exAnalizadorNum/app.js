77c69b21aead3d168d168c0516ff4f8303bda07e
Bryan Lucas da Conceição Afinovicz <bryanafinovicz706@gmail.com>
2026-03-26 11:50:59 -0300

    a principio, FINALIZADO

---
+++ exAnalizadorNum/app.js
@@ -0,0 +1,51 @@
+const form = document.querySelector("form");
+const divList = document.getElementById("lista");
+const btnFinalizar = document.getElementById("btnFinalizar");
+const btnLimpar = document.getElementById("btnLimpar");
+const response = document.getElementById("resultado");
+
+let list = [];
+
+function finish() {
+  const menorNum = Math.min(...list);
+  const maiorNum = Math.max(...list);
+  const media = somar() / list.length;
+
+  response.innerHTML += `<p> Ao todo, temos ${list.length} números cadastrados.</p>`;
+  response.innerHTML += `<p> O menor valor informado foi ${menorNum}.</p>`;
+  response.innerHTML += `<p> O maior valor informado foi ${maiorNum}.</p>`;
+  response.innerHTML += `<p> Somando todos os valores, temos ${somar()}.`;
+  response.innerHTML += `<p> A média dos valores é ${media}.</p>`;
+}
+
+function somar() {
+  let soma = 0;
+  for (let num in list) {
+    soma += list[num];
+  }
+  return soma;
+}
+
+function clearList() {
+  divList.innerHTML = "";
+  response.innerHTML = "";
+  list = [];
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const num = Number(document.getElementById("numero").value);
+
+  if (list.indexOf(num) === -1) {
+    list.push(num);
+    divList.innerHTML += `<li>Valor ${num} adicionado.</li>`;
+  } else {
+    alert("Esse numero já existe na lista");
+  }
+  // Limpar
+  document.getElementById("numero").value = "";
+  response.innerHTML = "";
+});
+
+btnFinalizar.addEventListener("click", finish);
+btnLimpar.addEventListener("click", clearList);
